2f074278a3d9f817a31ee3e8f761680a5fb4ad5c
Taimoor khan <Taimoorkhan311@gmail.com>
2024-05-23 02:10:57 +0500

    css changes

--- src/usePageVisibility.js
+++ src/usePageVisibility.js
@@ -4,14 +4,15 @@ const usePageVisibility = () => {
     useEffect( () => {
         const handleVisibilityChange = () => {
             if ( document.hidden && /Mobi|Android/i.test( navigator.userAgent ) ) {
-                document.body.style.visibility = "hidden";
+                document.body.classList.add( "background-hidden" );
             } else {
-                document.body.style.visibility = "visible";
+                document.body.classList.remove( "background-hidden" );
             }
         };
 
         document.addEventListener( "visibilitychange", handleVisibilityChange );
 
+        // Cleanup event listener on unmount
         return () => {
             document.removeEventListener( "visibilitychange", handleVisibilityChange );
         };
